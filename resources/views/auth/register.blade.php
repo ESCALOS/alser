b89443f1e35222fb8505e2f4edd9d592b5c8f622
Nanoka <stornblood6969@gmail.com>
2024-03-13 13:23:28 -0500

    Fixed inputs on register and login.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,23 +10,27 @@
             @csrf
 
             <div>
-                <x-input label="{{ __('Name') }}" id="name" class="block w-full mt-1" type="text" name="name"
-                    :value="old('name')" required autofocus autocomplete="name" />
+                <x-label for="name" value="{{ __('Name') }}" />
+                <x-input id="name" class="block w-full mt-1" type="text" name="name" :value="old('name')" required
+                    autofocus autocomplete="name" />
             </div>
 
             <div class="mt-4">
-                <x-input label="{{ __('Email') }}" id="email" class="block w-full mt-1" type="email"
-                    name="email" :value="old('email')" required autocomplete="username" />
+                <x-label for="email" value="{{ __('Email') }}" />
+                <x-input id="email" class="block w-full mt-1" type="email" name="email" :value="old('email')"
+                    required autocomplete="username" />
             </div>
 
             <div class="mt-4">
-                <x-input label="{{ __('Password') }}" id="password" class="block w-full mt-1" type="password"
-                    name="password" required autocomplete="new-password" />
+                <x-label for="password" value="{{ __('Password') }}" />
+                <x-input id="password" class="block w-full mt-1" type="password" name="password" required
+                    autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
-                <x-input label="{{ __('Confirm Password') }}" id="password_confirmation" class="block w-full mt-1"
-                    type="password" name="password_confirmation" required autocomplete="new-password" />
+                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                <x-input id="password_confirmation" class="block w-full mt-1" type="password"
+                    name="password_confirmation" required autocomplete="new-password" />
             </div>
 
             @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
