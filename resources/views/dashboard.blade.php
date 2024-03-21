f1630eeb600b7df7744432f8c50a4388223343ee
Nanoka <stornblood6969@gmail.com>
2024-03-21 15:04:39 -0500

    Quote was included in the dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+            {{ __('New operation') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
-            </div>
+        <div class="px-6 mx-auto max-w-7xl lg:px-8">
+            @include('partials.home.banner.quoter')
         </div>
     </div>
 </x-app-layout>
