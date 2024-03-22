84fe6dd09fb7bbbf08de4029e67ea2668f821a53
Nanoka <stornblood6969@gmail.com>
2024-03-22 15:31:15 -0500

    Added favicon to layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,10 +16,11 @@
         rel="stylesheet">
 
     <!-- Styles -->
-    @livewireStyles
+    <link rel="icon" href="http://127.0.0.1/storage/images/logo-negro.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @livewireStyles
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -18,7 +18,7 @@
         href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,651;1,651&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
         rel="stylesheet">
 
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="http://127.0.0.1/storage/images/logo-negro.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
