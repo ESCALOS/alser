61295a8c4079f8ab1bf80275f88efa38a0c81195
Nanoka <stornblood6969@gmail.com>
2024-03-22 21:39:41 -0500

    Fixed when username is null

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -42,7 +42,7 @@
                                 <span class="inline-flex rounded-md">
                                     <button type="button"
                                         class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md dark:text-gray-400 dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700">
-                                        {{ Auth::user()->name }}
+                                        {{ Auth::user()->name ?? 'Bienvenido' }}
 
                                         <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
                                             fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
