64537bdeb003a9f9f4f6857739de68ead9a55d32
Nanoka <stornblood6969@gmail.com>
2024-05-10 12:07:49 -0500

    Modified footer - company description

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -4,16 +4,17 @@
         <div class="grid max-w-screen-xl gap-12 px-6 py-10 mx-auto md:grid-cols-3">
             <div class="space-y-8 text-md">
                 <img src="{{ asset('images/logos/logo-alser.png') }}" alt="logo">
-                <p class="text-gray-300">
-                    Alser Cambio es una marca registrada de Alser Cambio Fintech SAC para comprar y vender dólares y
+                <p class="text-justify text-gray-300">
+                    Alser Cambio es una marca registrada de ALSER CAMBIO E.I.R.L para comprar y vender dólares y
                     cambiar soles y dólares online.
                 </p>
-                <p class="text-gray-300">
-                    Registrada como casa de cambio en la SBS (Número de Resolución: 01413-2019)
+                <p class="text-justify text-gray-300">
+                    Registrada como casa de cambio online en la SBS con número de resolución: 01234-2021
                 </p>
             </div>
             <div>
-                <h3 class="pb-2 mb-4 text-xl font-bold border-b-2 border-purple-600">MAPA DEL SITIO</h3>
+                <h3 class="pb-2 mb-4 text-xl font-bold border-b-2 border-purple-600">REGLAS Y CONDICIONES DEL SERVICIO
+                </h3>
                 <ul class="space-y-3 text-gray-300 text-md">
                     <li><a target="_blank" href="{{ route('policy.show') }}">Política de Privacidad</a></li>
                     <li><a target="_blank" href="{{ route('terms.show') }}">Términos y Condiciones</a></li>
@@ -27,13 +28,14 @@
             <div>
                 <h3 class="pb-2 mb-4 text-xl font-bold border-b-2 border-purple-600">CONTACTO</h3>
                 <ul class="space-y-4 text-gray-300">
-                    <li><i class="fa-regular fa-clock"></i> Lunes a Viernes de 8:00 a.m. a 8:00 p.m. y Sábado de 9:00
+                    <li><i class="mr-2 font-bold fa-regular fa-clock"></i> Lunes a Viernes de 8:00 a.m. a 8:00 p.m. y
+                        Sábado
+                        de 9:00
                         a.m. a
                         3:00 p.m.</li>
-                    <li><i class="fa-solid fa-location-dot"></i> Ca. Mártir Olaya 129. Of.: 1302. Miraflores, Lima.</li>
-                    <li><i class="fa-solid fa-phone"></i> (01) 4800648</li>
-                    <li><i class="fa-brands fa-whatsapp"></i> 51924901611</li>
-                    <li><i class="fa-regular fa-envelope"></i> info@alsercambio.com</li>
+                    <li><i class="mr-2 font-bold fa-solid fa-phone"></i> 960127206</li>
+                    <li><i class="mr-2 font-bold fa-brands fa-whatsapp"></i> 960127206</li>
+                    <li><i class="mr-2 font-bold fa-regular fa-envelope"></i> contactos@alsercambio.com</li>
                 </ul>
             </div>
         </div>
