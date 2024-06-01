9e8ca8697159bc8832b31358b600265e4eb7fdcf
Nanoka <stornblood6969@gmail.com>
2024-06-01 16:11:18 -0500

    "Rearranged HTML structure and moved x-operation components in pending.blade.php"

--- resources/views/livewire/operation/pending.blade.php
+++ resources/views/livewire/operation/pending.blade.php
@@ -1,16 +1,15 @@
 <div>
     <div class="w-full space-y-4 lg:w-1/3">
-        <div>
-            <x-operation.summary :$operation />
-        </div>
-        <div>
-            <x-operation.no-cash />
-        </div>
-        <div>
-            <x-operation.important :originBank="$operation->originBank->name" :destinationBank="$operation->destinationBank->name" />
-        </div>
+        <x-operation.summary :$operation />
+        <x-operation.no-cash />
+        <x-operation.important :originBank="$operation->originBank->name" :destinationBank="$operation->destinationBank->name" />
     </div>
     <div class="w-full lg:w-2/3">
+        <div>
 
+        </div>
+        <div>
+
+        </div>
     </div>
 </div>
